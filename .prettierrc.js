ba19decc96f55cf7ddd5b92e379cd08f7ea75c11
zc <761431522@qq.com>
2026-02-26 13:56:47 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+// Prettier 代码格式化配置
+module.exports = {
+  // 在语句末尾添加分号
+  semi: true,
+
+  // 使用双引号而不是单引号
+  singleQuote: false,
+
+  // 使用 2 个空格进行缩进
+  tabWidth: 2,
+
+  // 在 ES5 允许的地方添加尾随逗号
+  trailingComma: "es5",
+
+  // 每行代码的最大宽度为 80 个字符
+  printWidth: 80,
+
+  // 使用空格而不是制表符
+  useTabs: false,
+};
